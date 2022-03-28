0502c7121e322ebb902bd32ce351bb1bbd1045e3
Your Name <you@example.com>
2022-03-28 11:52:39 +0800

    owo

--- newton.cpp
+++ newton.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 double function(int array[],int size,int x){
 	int sum = 0,temp = 1;
-	for(int i=size-1,I=0;i>=0;i--,I++){
-		for(int j=1;j<=i;j++){
+	for(int i=0;i<=size-1;i++){
+		for(int j=1;j<=size-1-i;j++){
 			temp = temp*x;
 		}
-		sum = sum + temp*array[I];
+		sum = sum + temp*array[i];
 		temp = 1; 
 	}
 	return sum;
@@ -18,24 +18,21 @@ int main(){
 	cin>>Len;
 	for(int i=0;i<=Len-1;i++){
 		f[i] = 1;
-		X[i] = 1;
+	}
+	for(int i=0;i<=Len-1;i++){
 		cin>>F[i];
 	}
 	for(int i=Len-1,I=0;i>=0;i--,I++){
 		f[I] = F[I]*i;
 	}
+	X[0]=1;
 	for(int i=0;i<=100;i++){
-		if(function(f,Len,X[i]) == 0){
-			X[i+1] = X[i];
+		if(function(F,Len,X[i]) != 0){
+			X[i+1] = X[i] - (function(F,Len,X[i])/function(f,Len-1,X[i]));
+			cout<<X[i+1]<<endl;
+		}
+		else {
 			break;
-		} else {
-			X[i+1] = X[i] - (function(F,Len,X[i])/function(f,Len,X[i]));
 		}
 	}
-	for(int i=0;i<=100;i++){
-		cout<<X[i]<<endl;
-	}
-
-	
-
 } 
