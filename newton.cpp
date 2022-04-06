710679df4233cbbcc2d9f0c349e66188713ee1aa
Your Name <you@example.com>
2022-04-06 10:33:46 +0800

    owo

--- newton.cpp
+++ newton.cpp
@@ -13,26 +13,44 @@ double function(int array[],int size,int x){
 	return sum;
 }
 int main(){
+	system("chcp 65001");
 	int Len = 0,F[100],f[100];
 	double X[100];
-	cin>>Len;
-	for(int i=0;i<=Len-1;i++){
-		f[i] = 1;
-	}
-	for(int i=0;i<=Len-1;i++){
-		cin>>F[i];
-	}
-	for(int i=Len-1,I=0;i>=0;i--,I++){
-		f[I] = F[I]*i;
-	}
-	X[0]=1;
-	for(int i=0;i<=100;i++){
-		if(function(F,Len,X[i]) != 0){
-			X[i+1] = X[i] - (function(F,Len,X[i])/function(f,Len-1,X[i]));
-			cout<<X[i+1]<<endl;
+	while(true){
+		cout<<"輸入最高次數:";
+		cin>>Len;
+		Len++;
+		for(int i=0;i<=Len-1;i++){
+			f[i] = 1;
 		}
-		else {
+		for(int i=0;i<=Len-1;i++){
+			cout<<Len-i-1<<"次方之係數:";
+			cin>>F[i];
+		}
+		for(int i=Len-1,I=0;i>=0;i--,I++){
+			f[I] = F[I]*i;
+		}
+		X[0]=1;
+		int counter=0;
+		for(int i=0;i<=100;i++){
+			if(function(F,Len,X[i]) != 0){
+				X[i+1] = X[i] - (function(F,Len,X[i])/function(f,Len-1,X[i]));
+				counter++;
+			}
+			else {
+				break;
+			}
+		}
+		cout<<X[counter-1]<<endl;
+		cout<<"如果想重新請輸入1,結束輸入2"<<endl;
+		int a;
+		cin>>a;
+		if (a == 1){
+			continue;
+		}
+		else if (a == 2){
 			break;
 		}
 	}
+	
 } 
